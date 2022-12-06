1271b1599c62e025ba6301820d7954e95aa0c865
Ghliss <64869154+Ghliss@users.noreply.github.com>
2022-12-06 09:19:54 +0000

    Add files via upload

---
+++ Bitstream of an AI application/software/CW305 Design/cw_main.cpp
@@ -0,0 +1,305 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+/*#include <math.h>
+
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+//#include "tensorflow/lite/micro/examples/hello_world/hello_world_model_data.h"
+#include "model_data.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/testing/micro_test.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+
+
+//g_hello_world_model_data
+
+TF_LITE_MICRO_TESTS_BEGIN
+
+TF_LITE_MICRO_TEST(LoadModelAndPerformInference) {
+  // Define the input and the expected output
+  float x = 0.0f;
+  float y_true = sin(x);
+
+  // Set up logging
+  tflite::MicroErrorReporter micro_error_reporter;
+
+  // Map the model into a usable data structure. This doesn't involve any
+  // copying or parsing, it's a very lightweight operation.
+  const tflite::Model* model = ::tflite::GetModel(mobilenet_tflite);
+  if (model->version() != TFLITE_SCHEMA_VERSION) {
+    TF_LITE_REPORT_ERROR(&micro_error_reporter,
+                         "Model provided is schema version %d not equal "
+                         "to supported version %d.\n",
+                         model->version(), TFLITE_SCHEMA_VERSION);
+  }
+
+  // This pulls in all the operation implementations we need
+  tflite::AllOpsResolver resolver;
+
+  constexpr int kTensorArenaSize = 2000;
+  uint8_t tensor_arena[kTensorArenaSize];
+
+  // Build an interpreter to run the model with
+  tflite::MicroInterpreter interpreter(model, resolver, tensor_arena,
+                                       kTensorArenaSize, &micro_error_reporter);
+  // Allocate memory from the tensor_arena for the model's tensors
+  TF_LITE_MICRO_EXPECT_EQ(interpreter.AllocateTensors(), kTfLiteOk);
+
+  // Obtain a pointer to the model's input tensor
+  TfLiteTensor* input = interpreter.input(0);
+
+  // Make sure the input has the properties we expect
+  TF_LITE_MICRO_EXPECT(input != nullptr);
+  // The property "dims" tells us the tensor's shape. It has one element for
+  // each dimension. Our input is a 2D tensor containing 1 element, so "dims"
+  // should have size 2.
+  TF_LITE_MICRO_EXPECT_EQ(2, input->dims->size);
+  // The value of each element gives the length of the corresponding tensor.
+  // We should expect two single element tensors (one is contained within the
+  // other).
+  TF_LITE_MICRO_EXPECT_EQ(1, input->dims->data[0]);
+  TF_LITE_MICRO_EXPECT_EQ(1, input->dims->data[1]);
+  // The input is an 8 bit integer value
+  TF_LITE_MICRO_EXPECT_EQ(kTfLiteInt8, input->type);
+
+  // Get the input quantization parameters
+  float input_scale = input->params.scale;
+  int input_zero_point = input->params.zero_point;
+
+  // Quantize the input from floating-point to integer
+  int8_t x_quantized = x / input_scale + input_zero_point;
+  // Place the quantized input in the model's input tensor
+  input->data.int8[0] = x_quantized;
+
+  // Run the model and check that it succeeds
+  TfLiteStatus invoke_status = interpreter.Invoke();
+  TF_LITE_MICRO_EXPECT_EQ(kTfLiteOk, invoke_status);
+
+  // Obtain a pointer to the output tensor and make sure it has the
+  // properties we expect. It should be the same as the input tensor.
+  TfLiteTensor* output = interpreter.output(0);
+  TF_LITE_MICRO_EXPECT_EQ(2, output->dims->size);
+  TF_LITE_MICRO_EXPECT_EQ(1, output->dims->data[0]);
+  TF_LITE_MICRO_EXPECT_EQ(1, output->dims->data[1]);
+  TF_LITE_MICRO_EXPECT_EQ(kTfLiteInt8, output->type);
+
+  // Get the output quantization parameters
+  float output_scale = output->params.scale;
+  int output_zero_point = output->params.zero_point;
+
+  // Obtain the quantized output from model's output tensor
+  int8_t y_pred_quantized = output->data.int8[0];
+  // Dequantize the output from integer to floating-point
+  float y_pred = (y_pred_quantized - output_zero_point) * output_scale;
+
+  // Check if the output is within a small range of the expected output
+  float epsilon = 0.05f;
+  TF_LITE_MICRO_EXPECT_NEAR(y_true, y_pred, epsilon);
+
+  // Run inference on several more values and confirm the expected outputs
+  x = 1.f;
+  y_true = sin(x);
+  input->data.int8[0] = x / input_scale + input_zero_point;
+  interpreter.Invoke();
+  y_pred = (output->data.int8[0] - output_zero_point) * output_scale;
+  TF_LITE_MICRO_EXPECT_NEAR(y_true, y_pred, epsilon);
+
+  x = 3.f;
+  y_true = sin(x);
+  input->data.int8[0] = x / input_scale + input_zero_point;
+  interpreter.Invoke();
+  y_pred = (output->data.int8[0] - output_zero_point) * output_scale;
+  TF_LITE_MICRO_EXPECT_NEAR(y_true, y_pred, epsilon);
+
+  x = 5.f;
+  y_true = sin(x);
+  input->data.int8[0] = x / input_scale + input_zero_point;
+  interpreter.Invoke();
+  y_pred = (output->data.int8[0] - output_zero_point) * output_scale;
+  TF_LITE_MICRO_EXPECT_NEAR(y_true, y_pred, epsilon);
+}
+}*/
+
+
+/* Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+
+
+#include "tensorflow/lite/micro/examples/person_detection/main_functions.h"
+
+
+
+/*********************************************************/
+
+
+
+
+// Xilinx specific headers
+#include "xparameters.h"
+#include "xgpio.h"
+#include "xuartlite_l.h"
+
+#include "m3_for_arty.h"        // Project specific header
+#include "cw_gpio.h"
+#include "uart.h"
+#include "spi.h"
+#include "gpio.h"
+
+// simpleserial :
+#include "simpleserial.h"
+#include "hal.h"
+
+
+
+// uartlite example:
+#include "xstatus.h"
+#include "xuartlite.h"
+
+
+
+
+
+
+
+
+/************************************************************/
+
+
+
+#include "tensorflow/lite/micro/examples/person_detection/detection_responder.h"
+#include "tensorflow/lite/micro/examples/person_detection/image_provider.h"
+#include "tensorflow/lite/micro/examples/person_detection/model_settings.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
+#include "tensorflow/lite/micro/models/person_detect_model_data.h"
+#include "tensorflow/lite/micro/system_setup.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "C:/Users/Khairane/Desktop/resnet/resnet50.h"
+
+
+// Globals, used for compatibility with Arduino-style sketches.
+namespace {
+tflite::ErrorReporter* error_reporter = nullptr;
+const tflite::Model* model = nullptr;
+tflite::MicroInterpreter* interpreter = nullptr;
+TfLiteTensor* input = nullptr;
+
+// In order to use optimized tensorflow lite kernels, a signed int8_t quantized
+// model is preferred over the legacy unsigned model format. This means that
+// throughout this project, input images must be converted from unisgned to
+// signed format. The easiest and quickest way to convert from unsigned to
+// signed 8-bit integers is to subtract 128 from the unsigned value to get a
+// signed value.
+
+// An area of memory to use for input, output, and intermediate arrays.
+constexpr int kTensorArenaSize = 136 * 1024;
+static uint8_t tensor_arena[kTensorArenaSize];
+}  // namespace
+
+// The name of this function is important for Arduino compatibility.
+void setup() {
+	
+	//trigger_setup(); //defined to InitialiseGPIO
+	
+ // init_uart();	 //defined to InitialiseUART
+	
+  tflite::InitializeTarget();
+
+  // Set up logging. Google style is to avoid globals or statics because of
+  // lifetime uncertainty, but since this has a trivial destructor it's okay.
+  // NOLINTNEXTLINE(runtime-global-variables)
+  static tflite::MicroErrorReporter micro_error_reporter;
+  error_reporter = &micro_error_reporter;
+
+  // Map the model into a usable data structure. This doesn't involve any
+  // copying or parsing, it's a very lightweight operation.
+  model = tflite::GetModel(model_no_quant_tflite);
+  if (model->version() != TFLITE_SCHEMA_VERSION) {
+    TF_LITE_REPORT_ERROR(error_reporter,
+                         "Model provided is schema version %d not equal "
+                         "to supported version %d.",
+                         model->version(), TFLITE_SCHEMA_VERSION);
+    return;
+  }
+
+  // Pull in only the operation implementations we need.
+  // This relies on a complete list of all the ops needed by this graph.
+  // An easier approach is to just use the AllOpsResolver, but this will
+  // incur some penalty in code space for op implementations that are not
+  // needed by this graph.
+  //
+  //tflite::AllOpsResolver resolver;
+  // NOLINTNEXTLINE(runtime-global-variables)
+  static tflite::MicroMutableOpResolver<5> micro_op_resolver;
+  /*micro_op_resolver.AddAveragePool2D();
+  micro_op_resolver.AddConv2D(tflite::Register_CONV_2D_INT8());
+  micro_op_resolver.AddDepthwiseConv2D(
+      tflite::Register_DEPTHWISE_CONV_2D_INT8());
+  micro_op_resolver.AddReshape();
+  micro_op_resolver.AddSoftmax(tflite::Register_SOFTMAX_INT8());*/
+
+  // Build an interpreter to run the model with.
+  // NOLINTNEXTLINE(runtime-global-variables)
+  static tflite::MicroInterpreter static_interpreter(
+      model, micro_op_resolver, tensor_arena, kTensorArenaSize, error_reporter);
+  interpreter = &static_interpreter;
+
+  // Allocate memory from the tensor_arena for the model's tensors.
+  TfLiteStatus allocate_status = interpreter->AllocateTensors();
+  if (allocate_status != kTfLiteOk) {
+    TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
+    return;
+  }
+
+  // Get information about the memory area to use for the model's input.
+  input = interpreter->input(0);
+}
+
+// The name of this function is important for Arduino compatibility.
+void loop() {
+  // Get image from provider.
+  if (kTfLiteOk != GetImage(error_reporter, kNumCols, kNumRows, kNumChannels,
+                            input->data.int8)) {
+    TF_LITE_REPORT_ERROR(error_reporter, "Image capture failed.");
+  }
+
+  // Run the model on this input and make sure it succeeds.
+  if (kTfLiteOk != interpreter->Invoke()) {
+    TF_LITE_REPORT_ERROR(error_reporter, "Invoke failed.");
+  }
+  XGpio_DiscreteWrite(&Gpio_TrigOut, TRIG_CHANNEL, 1);
+	//trigger_high();
+  TfLiteTensor* output = interpreter->output(0);
+	//trigger_low();
+  XGpio_DiscreteWrite(&Gpio_TrigOut, TRIG_CHANNEL, 0);
+
+  // Process the inference results.
+  //int8_t person_score = output->data.uint8[kPersonIndex];
+  //int8_t no_person_score = output->data.uint8[kNotAPersonIndex];
+ // RespondToDetection(error_reporter, person_score, no_person_score);
+}
+
+int main()
+{setup();
+	loop();}
